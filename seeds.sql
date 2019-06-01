2c7d6cb08d4a58380ebadf70bda01eb4e878d107
Ben Miller <mail@benkmiller.com>
2019-06-01 15:50:38 -0400

    Created products table schema

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE bamazon;
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+('Basketball', 'Sports & Outdoors', 29.00, 10),
+('iPhone Case', 'Electronics & Accessories', 10.00, 9),
+('Pencils', 'Home Office & Small Business', 12.98, 8),
+('Nintendo Switch', 'Electronics & Accessories', 298.96, 7),
+('Watch', ' Clothing, Shoes, & Jewelry', 328.98, 6),
+('Power Drill', 'Tools & Home Improvement', 79.99, 5),
+('$50 Bamazon Gift Card', 'Gift Cards', 50.00, 4),
+('Comb', 'Beauty & Personal Care', 22.00, 3),
+('55-inch 4K Smart TV', 'Electronics & Accessories', 329.99, 2),
+('Laptop Computer', 'Electronics & Accessories', 2500.00, 1)
+;
